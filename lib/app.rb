b1aec821e99de2324788bd60233e74ac22ad9677
cesare camurani <cesare.camurani@gmail.com>
2018-11-12 16:24:50 +0000

    Start and Restart Sinatra

---
+++ lib/app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'shotgun'
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "Ciao Cesare!"
+end
+
+get '/dunnot_touch!' do
+  "For my eyes only"
+end
+
+get '/victoria_secret' do
+  "Ciao Luca Francesco Eto!"
+end
